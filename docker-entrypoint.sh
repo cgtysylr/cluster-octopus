01b5013aa5cbfe28544066eca8a3e1840082b0e4
Çağatay Hakan SÖYLER <cagatay.soyler@mikrogrup.com>
2025-01-08 10:35:06 +0300

    entrypoint changed.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+echo "Creating necessary directories..."
+mkdir -p storage/framework/views &&
+mkdir -p storage/framework/cache &&
+chmod -R 775 storage/framework
+
 echo "Waiting for database to be ready..."
 sleep 10
 
@@ -10,3 +15,5 @@ php artisan migrate --force
 
 echo "Starting Laravel server..."
 exec "$@"
+
+
