d1091b580e98e1b32ecf7d3698674d7fb49d8d18
Çağatay SÖYLER <c.soyler@teamsystem.com>
2025-02-18 08:07:05 +0300

    fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
 set -e
+echo "Starting entrypoint script..."
 
-echo "Creating necessary directories..."
-mkdir -p storage/framework/views &&
-mkdir -p storage/framework/cache &&
-chmod -R 775 storage/framework
+
+echo "Clearing cache..."
+php artisan config:clear
+
+#echo "Creating necessary directories..."
+#mkdir -p storage/framework/views &&
+#mkdir -p storage/framework/cache &&
+#chmod -R 775 storage/framework
 
 echo "Waiting for database to be ready..."
-sleep 10
+sleep 5
 
 echo "Running migrations..."
 php artisan migrate --force
