f6882eda632e4230b989e7e41ac662c3ba96fe60
Çağatay SÖYLER <c.soyler@teamsystem.com>
2025-02-18 08:52:30 +0300

    feat: Add NetworkConnection model
     commit adds a new NetworkConnection model the app. The model includes fields for source destination, port will trigger a release bumping a MINOR version.

---
+++ app/Models/NetworkConnection.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class NetworkConnection extends Model
+{
+    use HasFactory;
+
+    protected $table = 'network_connections';
+
+    protected $fillable = [
+        'source',
+        'destination',
+        'port',
+        'status',
+        'description'
+    ];
+}
