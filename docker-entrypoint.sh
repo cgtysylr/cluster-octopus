14d2e39d8c3df2e3d9fe05d8283c46bef672d395
Çağatay SÖYLER <c.soyler@teamsystem.com>
2025-03-02 15:06:29 +0300

    feat: Remove commented-out directory creation in entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,11 +6,6 @@ echo "Starting entrypoint script..."
 echo "Clearing cache..."
 php artisan config:clear
 
-#echo "Creating necessary directories..."
-#mkdir -p storage/framework/views &&
-#mkdir -p storage/framework/cache &&
-#chmod -R 775 storage/framework
-
 echo "Waiting for database to be ready..."
 sleep 5
 
