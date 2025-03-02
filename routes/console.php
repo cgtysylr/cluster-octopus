00db5675107834bfde10b0f8edf5e60ee50f0a01
Çağatay SÖYLER <c.soyler@teamsystem.com>
2025-03-02 15:43:02 +0300

    feat: Append output of kubernetes:check-connections command to log file

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-app(Schedule::class)->command('kubernetes:check-connections')->everyMinute();
+app(Schedule::class)
+    ->command('kubernetes:check-connections')
+    ->everyMinute()
+    ->appendOutputTo(storage_path('logs/k8s_check_connections.log'));;
